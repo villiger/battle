b03e5cb2a9fff41a048e565a6fe4021507cf0572
Andy Villiger <a.villiger@gmail.com>
2014-03-22 15:33:24 +0100

    added RedBean dependency and configuration file

--- index.php
+++ index.php
@@ -1,17 +1,28 @@
 <?php
 
 require 'vendor/autoload.php';
+use RedBean_Facade as R;
+
+if (! file_exists('config.php')) {
+    die("Please copy 'config.php.dist' to 'config.php' and set all values properly.");
+}
+
+require 'config.php';
 
 $app = new \Slim\Slim();
 
 $app->config(array(
-    'debug' => true,
+    'debug' => DEBUG,
     'templates.path' => 'templates',
     'view' => '\Slim\LayoutView',
     'layout' => 'layout/base.php'
 ));
 
+R::setup(DB_PATH, DB_USER, DB_PASS);
+
 require 'routes/index.php';
 require 'routes/game.php';
 
 $app->run();
+
+R::close();
