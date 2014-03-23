b834b01ed40f8f62f8928aad8a869feb457967de
joalder <jonas.alder@gmail.com>
2014-03-23 21:59:00 +0100

    fixed vagrant provisioning
    mysqld still seems not to install correctly

--- vagrant_init.sh
+++ vagrant_init.sh
@@ -1,17 +1,27 @@
+# Refresh packages
+apt-get update
+
 # Server software
 apt-get install -y apache2
-apt-get install -y php5.6
-apt-get install -y mysql
-
-# Dev libs and tools
-curl -sS https://getcomposer.org/installer | php
+apt-get install -y php5
+apt-get install -y php5-json
+apt-get install -y mysql-server
 
 # Additional utilities
 apt-get install -y htop
+apt-get install -y curl
+apt-get install -y git
 
 
 # Configure stuff? i.e. apache
 rm -rf /var/www
 ln -fs /vagrant /var/www
 
-php /vagrant/composer.json install
+cd /vagrant
+
+ln -s config.php config.php.dist
+
+# Dev libs and tools
+curl -sS https://getcomposer.org/installer | php
+
+php /vagrant/composer.phar install
