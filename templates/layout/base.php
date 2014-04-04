2de0de5e9a627bea079099059ee3f799e6959356
joalder <jonas.alder@gmail.com>
2014-04-05 00:19:39 +0200

    AngularJS and some login form

--- templates/layout/base.php
+++ templates/layout/base.php
@@ -1,11 +1,15 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" ng-app="battleApp">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
     <title>Battle Chess</title>
     <link rel="stylesheet" href="/css/style.css">
+    <script src="/js/jquery-1.11.0.min.js"></script>
     <script src="/js/bootstrap.min.js"></script>
+    <script src="/js/angular.min.js"></script>
+    <script src="/js/angular-route.min.js"></script>
+    <script src="/app/js/app.js"></script>
 </head>
 <body>
     <div class="main container">
