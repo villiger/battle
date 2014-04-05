728edcdf0d2b11a9739ea1ec8a7ae9e1f2148ed8
joalder <jonas.alder@gmail.com>
2014-04-05 19:22:45 +0200

    Enabled mod_rewrite in vagrant

--- vagrant_init.sh
+++ vagrant_init.sh
@@ -9,6 +9,7 @@ apt-get install -q -y apache2
 apt-get install -q -y php5
 apt-get install -q -y php5-json
 apt-get install -q -y php5-mysql
+apt-get install -q -y php5-curl
 apt-get install -q -y mysql-server
 
 # Additional utilities
@@ -18,8 +19,18 @@ apt-get install -q -y git
 
 
 # Configure stuff? i.e. apache
+# Apache
 rm -rf /var/www
 ln -fs /vagrant /var/www
+
+# Enable mod_rewrite http://www.dev-metal.com/enable-mod_rewrite-ubuntu-12-04-lts/
+sudo a2enmod rewrite
+sudo service apache2 restart
+sudo cp /etc/apache2/apache2.conf /etc/apache2/apache2.conf.backup
+sudo cat /etc/apache2/apache2.conf | sed 's/AllowOverride None/AllowOverride All/g' > /etc/apache2/apache2.conf.new
+sudo cp /etc/apache2/apache2.conf.new /etc/apache2/apache2.conf
+sudo service apache2 restart
+
 mysqladmin create -uroot battle
 
 cd /vagrant
