e7fe58a9f7b4bcbb94c439120a9c72b74e2634e4
Andy Villiger <a.villiger@gmail.com>
2014-05-12 23:11:53 +0200

    table sizing fix

--- templates/game.php
+++ templates/game.php
@@ -3,12 +3,14 @@
 $field = $game->getField();
 ?>
 
-<table class="field" style="width: <?= $field->getWidth() * 80 ?>px;">
+<table class="field">
     <tbody>
         <?php for($row = 0; $row < $field->getHeight(); $row++): ?>
             <tr>
                 <?php for($column = 0; $column < $field->getWidth(); $column++): ?>
-                    <td class="tile <?= $field->getTileType($row, $column) ?>">&nbsp;</td>
+                    <td class="tile <?= $field->getTileType($row, $column) ?>">
+                        <div class="sizer"></div>
+                    </td>
                 <?php endfor; ?>
             </tr>
         <?php endfor; ?>
