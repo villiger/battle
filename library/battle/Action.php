a0d6172284c902a2db3748fbd9400690987de9e9
joalder <jonas.alder@gmail.com>
2014-05-14 19:50:00 +0200

    Added basic Action class

---
+++ library/battle/Action.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace battle;
+
+class Action {
+	const TYPE_MESSAGE = 0;
+	const TYPE_ATTACK = 1;
+	const TYPE_MOVE = 2;
+
+	private $player;
+	private $game;
+	private $payload;
+
+
+	public static function load($id){
+		$action = \R::load('action', $id)
+		if(! $action){
+			throw new \Exception("Action with id '$id' not found!")
+		}
+		return $action
+	}
+
+	public static function create($type, $player, $game, $payload){
+		// @TODO: write it :)
+	}
+
+}
