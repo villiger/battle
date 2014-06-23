51b2a365dc1c192caa2e86be5daaf2c3685f5aad
Andy Villiger <a.villiger@gmail.com>
2014-06-23 22:19:32 +0200

    screenshot on welcome page

--- templates/index.php
+++ templates/index.php
@@ -1,5 +1,9 @@
 <div class="welcome">
+
+
     <h1>Welcome to Battle Chess!</h1>
+
+    <img src="/img/screenshots/screenshot1.png" alt="Screenshot of the game" class="title-screen">
 
     <a href="/login/facebook">
         <p><img src="/img/facebook.png" alt="Facebook Logo"></p>
